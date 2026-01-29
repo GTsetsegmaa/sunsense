59a41267beaeb4f2b7de3bf8cce31e9764d28577
GTsetsegmaa <gtsetsegm@gmail.com>
2026-01-28 19:42:25 -0800

    improve daily appliance energy use calculator

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 def main():
     print("Hello from sunsense!")
 
-def daily_energy(watts, hours):
-    daily = (watts * hours) / 1000
+def daily_energy_appliance(appliance_watts, hours):
+    return (watts * hours) / 1000
 
 if __name__ == "__main__":
     main()
