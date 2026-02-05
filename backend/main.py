ef876c331100998d45c03aab5fd5eec85a58fca4
GTsetsegmaa <gtsetsegm@gmail.com>
2026-02-04 20:58:11 -0800

    main

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ def main():
     print("Hello from sunsense!")
 
 def daily_energy_appliance(appliance_watts, hours):
-    return (watts * hours) / 1000
+    return (appliance_watts * hours) / 1000
 
 if __name__ == "__main__":
     main()
